ab2b437bd6b4b1b0e698138f4e0fa1bf4287b161
Mateus Henrique <mateushh213@gmail.com>
2023-03-07 19:12:19 -0300

    commit

--- exerciciosjava/exerciciosjava/src/exercicio1.java
+++ exerciciosjava/exerciciosjava/src/exercicio1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class exercicio1 {
     public static void main(String[] args) throws Exception {
         Scanner entrada = new Scanner(System.in);
         int[] valores = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -9,18 +9,13 @@ public class App {
         System.out.println("------------------");
         if (escolha == 0) {
             for (int i : valores) {
-                if(i % 2 == 0){
-                    System.out.println(i);
-                }
+                if(i % 2 == 0){System.out.println(i);}
             }    
         }if (escolha == 1) {
             for (int i2 : valores) {
-                if(i2 % 2 != 0){
-                    System.out.println(i2);
-                }
+                if(i2 % 2 != 0){System.out.println(i2);}
             }
         }
-        
         entrada.close();
     }
 }
