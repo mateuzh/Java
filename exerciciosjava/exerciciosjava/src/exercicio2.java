036af8c02b8ccf7dd9c017652a521cf77c2717cb
Mateus Henrique <mateushh213@gmail.com>
2023-03-07 21:14:29 -0300

    commit

--- exerciciosjava/exerciciosjava/src/exercicio2.java
+++ exerciciosjava/exerciciosjava/src/exercicio2.java
@@ -4,8 +4,12 @@ public class exercicio2 {
     public static void main(String[] args) throws Exception {
         
         
-        
+        //Inicializa a captura de dados digitados pelo usuário
         Scanner entrada = new Scanner(System.in);
+        float ipi;
+
+        System.out.print("Digite o valor do IPI: ");
+        ipi = entrada.nextFloat();
 
         System.out.print("Código do produto: ");
         int codigoProd1 = entrada.nextInt();
@@ -20,12 +24,20 @@ public class exercicio2 {
         float valorUnitario2 = entrada.nextFloat();
         System.out.print("Quantidade:");
         int quantidade2 = entrada.nextInt();
-        
-        double valorTotal = (valorUnitario1 * quantidade1 + valorUnitario2 * quantidade2)* (24.75/100+1);
-        
-        System.out.print("Total: R$"+ valorTotal);
 
+        float valorTotal = (valorUnitario1 * quantidade1 + valorUnitario2 * quantidade2) * (ipi/100+1);
 
+        System.out.print(
+            "produto 1 " + codigoProd1 + " -> Valor: " + valorUnitario1 + " Quantidade: " + quantidade1 +
+            "\nproduto 2 " + codigoProd2 + " -> Valor: " + valorUnitario2 + " Quantidade: " + quantidade2 +
+            "\nTotal: R$%.2f" + valorTotal 
+        );
+        
+        
+        
+        
+
+        //Finaliza a variável entrada.
         entrada.close();
     }
 }
